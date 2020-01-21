7250646b9b948ccbddae0e2182d08f0b3dbb3a61
Ron Martin <ronmartin89@gmail.com>
2020-01-21 13:44:53 -0800

    Working through the Expanded page render

--- src/index.js
+++ src/index.js
@@ -19,36 +19,36 @@ import store from './store.js';
 //     }); 
 // });
 
-api.getBookmarks()
-  .then(resp => resp.json)
-  .then(items =>
-    console.log(items))
-  .catch((error) => {
-    console.log(error);
-    //store.setError(error.message);
-    //renderError();
-  });
+// api.getBookmarks()
+//   .then(resp => resp.json)
+//   .then(items =>
+//     console.log(items))
+//   .catch((error) => {
+//     console.log(error);
+//     //store.setError(error.message);
+//     //renderError();
+//   });
 
 
-let newBookmarkName = 'google';
-let newUrlName = 'https://www.google.com';
-let newDescription = 'search entginee';
-let newRating = 5;
+// let newBookmarkName = 'google';
+// let newUrlName = 'https://www.google.com';
+// let newDescription = 'search entginee';
+// let newRating = 5;
 
-const newBookmarkEntry = {
-  title: newBookmarkName,
-  url: newUrlName,
-  desc: newDescription,
-  rating: newRating
-};
+// const newBookmarkEntry = {
+//   title: newBookmarkName,
+//   url: newUrlName,
+//   desc: newDescription,
+//   rating: newRating
+// };
 
-api.createBookmark(newBookmarkEntry)
-  .then(resp => console.log(resp))
-  .catch((error) => {
-    console.log(`This is the error: ${error.message}`);
-    store.setError(error.message);
-    bookmarksApp.renderError();
-  });
+// api.createBookmark(newBookmarkEntry)
+//   .then(resp => console.log(resp))
+//   .catch((error) => {
+//     console.log(`This is the error: ${error.message}`);
+//     store.setError(error.message);
+//     bookmarksApp.renderError();
+//   });
 
 
 const main = function() {
