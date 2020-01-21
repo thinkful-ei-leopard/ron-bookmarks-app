ac6bb68f86c10adaad2d70e2d04f210d7fb20aac
Ron Martin <ronmartin89@gmail.com>
2020-01-21 10:01:44 -0800

    Working on the stars for the Add Bookmarks View

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
 'use strict';  // eslint says strict mode is unnecessary inside modules
 
+$(document).ready(function(){
+  // Check Radio-box
+  $(".rating input:radio").attr("checked", false);
+
+  $('.rating input').click(function () {
+      $(".rating span").removeClass('checked');
+      $(this).parent().addClass('checked');
+  });
+
+  $('input:radio').change(
+    function(){
+      var userRating = this.value;
+      alert(userRating);
+  }); 
+});
+
 const main = function() {
   console.log('testing 123');
 };
