3e731e2ab4b9eb3990b8d59669a1d3903b160a8c
Ron Martin <ronmartin89@gmail.com>
2020-01-21 15:26:40 -0800

    Add New Bookmark page now loads when New button is clicked

--- src/bookmarksApp.js
+++ src/bookmarksApp.js
@@ -54,6 +54,34 @@ const generateItemElement = function (item) {
   
 };
 
+const generateAddBookmarkView = function() {
+  return `    <form>
+  <i class="js-item-close fas fa-times"></i>
+  <h2>Add New Bookmark</h2> <!-- delete this after-->
+
+  <label for="new-bookmark">Add New Bookmark</label>
+  <input type="text" id="new-bookmark" name="add-bookmark" required minlength="4" size="20" placeholder="URL here" required>
+  <fieldset>
+      <label for="name-new-bookmark"></label>
+      <input type="text" id="name-new-bookmark" name="title-bookmark" required minlength="4" size="20" placeholder="Title of Bookmark here" required>
+
+
+      <div class="rating">
+          <span><input type="radio" name="rating" id="str5" value="5"><label for="str5"></label></span>
+          <span><input type="radio" name="rating" id="str4" value="4"><label for="str4"></label></span>
+          <span><input type="radio" name="rating" id="str3" value="3"><label for="str3"></label></span>
+          <span><input type="radio" name="rating" id="str2" value="2"><label for="str2"></label></span>
+          <span><input type="radio" name="rating" id="str1" value="1"><label for="str1"></label></span>
+      </div>
+
+      <label for="description-text"></label>
+      <textarea id="description-text" name="bookmark-description" rows="5" cols="33" placeholder="Type your description here..."></textarea>
+      
+  </fieldset>
+  <button>Create</button>
+</form>`;
+};
+
 const generateBookmarksString = function (bookmarksList) {
   const items = bookmarksList.map((item) => generateItemElement(item));
   return items.join('');
@@ -74,7 +102,16 @@ const handleCloseError = function () {
   });
 };
 
-const handleNewItemClicked = function () {};
+const handleNewItemClicked = function () {
+  // This will hide the current views not needed
+  // show the correct view to add a new bookmark
+  $('#new-bookmark-button').on('click', event => {
+    console.log('new bookmark clicked');
+    $('.js-bookmarks-list').addClass('hidden');
+    $('.init-view').addClass('hidden');
+    $('.container').html(generateAddBookmarkView);
+  });
+};
 
 const handleFilterClicked = function() {};
 
@@ -98,6 +135,11 @@ const handleDeleteBookmarkClicked = function() {
       .then(() => {
         store.findAndDelete(id);
         render();
+      })
+      .catch((error) => {
+        console.log(`This is the error: ${error.message}`);
+        store.setError(error.message);
+        renderError();
       });
   });
 
@@ -106,7 +148,11 @@ const handleDeleteBookmarkClicked = function() {
 
 const handleCloseViewClicked = function() {};
 
-const handleCreateSubmit = function() {};
+const handleCreateSubmit = function() {
+  // this will create a POST request from the formdata()
+  // get the response and put that into store
+  // and render the page correctly.
+};
 
 const bindEventListeners = function() {
   handleNewItemClicked();
